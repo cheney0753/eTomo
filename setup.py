4662f9686f1d85adc860a4815144d8f4b09aa7be
Zhichao Zhong <zhchzhong@gmail.com>
2018-04-25 16:05:37 +0200

    init the setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(name = 'astra-et',
+      version = '0.1',
+      description = 'The astra toolkit for electron tomography.',
+      author = 'Zhichao Zhong',
+      author_email = 'zhong@cwi.nl',
+      license = license,
+      packages = find_packages( include = ('astra-et',), exclude =('tests','data', 'samples'))
+      )
+
